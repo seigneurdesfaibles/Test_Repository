cfe2a69e6f5b3aceb9f8f0dc498c063d8adb55c3
seigneurdesfaibles <163987626+seigneurdesfaibles@users.noreply.github.com>
2024-10-24 22:29:25 +0300

    ООП ПР№4
    ООП ПР№4

---
+++ ООП ПР№4.1.cs
@@ -0,0 +1,44 @@
+﻿using System.Xml.Serialization;
+namespace Example2
+{
+    public class Person
+    {
+        public string Name { get; set; } = "Undefined"; public int Age { get; set; } = 1;
+        public Person() { }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[]
+            {
+                new Person("Tom", 37),
+                new Person("Bob", 46)
+            };
+            string filepath = "C:\\Users\\User\\OneDrive\\Рабочий стол\\gh.txt";
+            XmlSerializer formatter = new XmlSerializer(typeof(Person[]));
+            // збереження масиву у файл
+            using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, people);
+            }
+            // відновлення масиву із файлу
+            using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate))
+            {
+                Person[]? newpeople = formatter.Deserialize(fs) as Person[];
+                if (newpeople != null)
+                {
+                    foreach (Person person in newpeople)
+                    {
+                        Console.WriteLine($"Name: {person.Name} --- Age: {person.Age}");
+                    }
+                }
+            }
+        }
+    }
+} 
---
+++ ООП ПР№4.2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Practice04
+{
+    public class Person
+    {
+        public string Name { get; set; } = "Undefined";
+        public int Age { get; set; } = 1;
+
+        public Person() { }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    class class1
+    {
+        static void Main(string[] args)
+        {
+            // Створюємо об'єкт Person
+            Person person = new Person("Tom", 37);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            // Визначаємо шлях до файлу
+            string filepath = "C:\\Users\\User\\OneDrive\\Рабочий стол\\op.txt";
+
+            // Серіалізація об'єкта в XML і запис у файл
+            using (FileStream fs = new FileStream(filepath, FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, person);
+                Console.WriteLine("Object has been serialized");
+            }
+
+            // Десеріалізація об'єкта з файлу XML
+            using (FileStream fs = new FileStream(filepath, FileMode.Open))
+            {
+                Person dperson = xmlSerializer.Deserialize(fs) as Person;
+                if (dperson != null)
+                {
+                    Console.WriteLine($"Name: {dperson.Name}  Age: {dperson.Age}");
+                }
+            }
+        }
+    }
+}
